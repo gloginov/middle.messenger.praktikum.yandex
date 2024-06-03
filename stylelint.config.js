e64cf0204543e5ced25e9a323e6714ceb440d377
gloginov <georgeloginov90@gmail.com>
2024-06-03 11:20:08 +0500

    [practicum_2] *add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-html',
+    'stylelint-config-recommended-vue',
+    'stylelint-config-prettier',
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {},
+};
